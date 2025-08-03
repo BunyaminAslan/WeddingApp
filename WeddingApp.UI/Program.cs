eed6a84a0f1fedbaa1c6fc00212cef0fa757b55a
CBA_RS <bunyamin.aslaan@gmail.com>
2025-08-03 17:36:20 +0300

    x

--- WeddingApp.UI/Program.cs
+++ WeddingApp.UI/Program.cs
@@ -99,6 +99,12 @@ builder.Services.AddSingleton<IUploadQueue>(sp => {
 
 builder.Services.AddSingleton<IRedisQueueService, RedisQueueService>();
 
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MinRequestBodyDataRate = null;
+});
+
 var app = builder.Build();
 
 //app.UseSerilogRequestLogging();
